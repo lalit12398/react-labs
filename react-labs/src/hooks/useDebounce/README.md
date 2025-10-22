e18e787e5fced149aaafd39f6aff84413db94d78
Lalit Chauhan <lchauhan@workforcesoftware.com>
2025-10-22 23:37:24 +0530

    feature: add debounce hook with example

---
+++ react-labs/src/hooks/useDebounce/README.md
@@ -0,0 +1,20 @@
+# useDebounce Hook
+
+Simple react hook to delay execution after specified 'delay'
+
+## Features
+
+-   Delay execution by certain 'delay' ms
+-   Timeout cleanup on dependency changes
+-   works with any value (string)
+
+## Usecase
+
+-   Search box
+-   Filters
+-   Performance optimisation
+
+## Learnings
+
+-   React cleanup runs before the next effect.
+-   Debouncing reduces unnecessary API calls.
---
+++ react-labs/src/hooks/useDebounce/example.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react'
+import { useDebounce } from '.'
+
+interface IPosts {
+    body: string
+    id: number
+    title: string
+    userId: number
+}
+
+function SearchBox() {
+    const [posts, setPosts] = useState<IPosts[] | []>([])
+    const [searchTerm, setSearchTerm] = useState('')
+
+    const debouncedSearchTerm = useDebounce(searchTerm, 3000)
+
+    //dummy fetch for posts on change of input after a certain delay
+    useEffect(() => {
+        fetch(
+            `https://jsonplaceholder.typicode.com/posts?title=${debouncedSearchTerm}`
+        )
+            .then((res) => res.json())
+            .then((res) => setPosts(res))
+    }, [debouncedSearchTerm])
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchTerm(e.target.value)
+    }
+    return (
+        <>
+            <div>
+                <p>useDebounce example</p>
+                <input
+                    type="text"
+                    onChange={(e) => handleChange(e)}
+                    value={searchTerm}
+                />
+                {posts.map((post) => (
+                    <p>{post.title}</p>
+                ))}
+            </div>
+        </>
+    )
+}
+
+export default SearchBox
